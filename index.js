285658c8e7653175081a275c47ade0df2546da73
Marcin Czekajski <czekajskimarcin@gmail.com>
2023-11-22 21:40:49 +0100

    ask a question and log an answer

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ const openai = new OpenAI({
 
 async function main() {
   const chatCompletion = await openai.chat.completions.create({
-    messages: [{ role: "user", content: "Say this is a test" }],
+    messages: [
+      { role: "user", content: "What is the biggest city in Poland?" },
+    ],
     model: "gpt-3.5-turbo",
   });
+
+  console.log(chatCompletion.choices[0].message);
 }
 
 main();
