534e3233cb65a7907b65f08d21563488ceb51cae
Marcin Czekajski <czekajskimarcin@gmail.com>
2023-11-22 21:03:51 +0100

    dotenv as module

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-require("dotenv").config();
-const { Configuration, OpenAIApi } = require("openai");
+import OpenAI from "openai";
+import "dotenv/config";
 
-const config = new Configuration({
+const openai = new OpenAI({
   apiKey: process.env.OPENAI_KEY,
 });
+
+async function main() {
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: "user", content: "Say this is a test" }],
+    model: "gpt-3.5-turbo",
+  });
+}
+
+main();
